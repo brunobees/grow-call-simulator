ab8a881890a226ba5abe707b769cdc525ede9f51
Bruno Costa Candia <bruno.candia@ab-inbev.com>
2024-02-27 16:13:44 -0300

    Your commit message here

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Mail } from "@/components/mail"
+import { accounts, mails } from "@/data"
+import './index.css'
+
+const defaultLayout = undefined
+const defaultCollapsed = undefined
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+   <App
+      accounts={accounts}
+      mails={mails}
+      defaultLayout={defaultLayout}
+      defaultCollapsed={defaultCollapsed}
+      navCollapsedSize={4}
+    />
+  </React.StrictMode>,
+)
+
+postMessage({ payload: 'removeLoading' }, '*')
